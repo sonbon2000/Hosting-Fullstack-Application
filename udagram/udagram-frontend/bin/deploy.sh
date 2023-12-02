f2bffc4612321519d1e89c15502b26c979e09c1f
sonbon2000 <sonbon1101@gmail.com>
2023-12-02 12:49:23 +0700

    deploy fe

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://my-bucket-18061995-udagram/  
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-bucket-18061995-udagram/
+aws s3 cp --recursive --acl public-read ./www s3://sontsh-udagram/  
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://sontsh-udagram/
